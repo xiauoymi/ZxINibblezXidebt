0b9dd6880b55ca78f72862d1083731428411c98e
Logic Reaktor <rocky.alam@gmail.com>
2016-03-12 21:07:26 -0600

    Added nibble-payments project to nibble parent.

---
+++ nibble-web/src/main/java/com/nibbledebt/core/processor/BillingProcessor.java
@@ -0,0 +1,38 @@
+/**
+ * Copyright 2015-2017. All rights reserved by Nibbledebt Inc.
+ */
+package com.nibbledebt.core.processor;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import com.nibbledebt.core.data.dao.INibblerDao;
+import com.nibbledebt.core.data.error.RepositoryException;
+import com.nibbledebt.core.data.model.Nibbler;
+
+/**
+ * @author Rocky Alam
+ *
+ */
+@Component
+public class BillingProcessor extends AbstractProcessor {
+	@Autowired
+	private INibblerDao nibblerDao;
+	
+
+	@Scheduled(cron="0 0 * * 1")
+	public void processWeeklyPayment() throws RepositoryException{
+		List<Nibbler> nibblers = nibblerDao.findAll();
+		for(Nibbler nibbler : nibblers){
+			
+		}
+	}
+	
+	public void processWeeklyPaymentForUser(Nibbler nibbler){
+		
+	}
+	
+}
